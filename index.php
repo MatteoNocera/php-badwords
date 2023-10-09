df3e9309a6f5e20787397513002c5bc7264fa227
MatteoNocera <matteo_nocera@tiscali.it>
2023-10-09 16:05:30 +0200

    create files

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words</title>
+</head>
+
+<body>
+    <h1>The Bad Word</h1>
+    <form action="script.php" method="POST">
+        <input type="text" name="paragraph" id="paragraph" placeholder="type here text">
+        <input type="text" name="bad_word" id="bad_word" placeholder="insert bad word">
+        <button type="submit">Send</button>
+    </form>
+</body>
+
+</html>
---
+++ script.php
@@ -0,0 +1,11 @@
+<!-- 
+    Descrizione:
+Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.
+Gestire il tutto con due file diversi.
+index.php
+script.php
+Il primo file dovrà permettere all’utente di inserire i dati e inviare la richiesta al server tramite il form (occhio a quato detto stamattina sui form e gli attributi necessari).
+Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
+ stampare a schermo il paragrafo e la sua lunghezza
+stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare
+ -->
