981c535c7b6d9bc93934f6fcda9d52341822c08b
MatteoNocera <matteo_nocera@tiscali.it>
2023-10-09 17:14:12 +0200

    use substr_replace and str_repeat to complete ex

--- script.php
+++ script.php
@@ -16,8 +16,41 @@ $paragraph = $_POST['paragraph'];
 
 $paragraph_leng = strlen($paragraph);
 
+$bad_word = $_POST['bad_word'];
+
+// Utilizzo count per determinare la lunghezza della parola
+$count = strlen($bad_word) - 1;
+
+//utilizzo substr_replace per rimpiazzare le lettere finali dalla parola (uso il contatore negativo per partire dalla fine) ed utilizzo repeat, 3 per aggiungere i 3 asterischi
+$hidden_bad_word = substr_replace($bad_word, str_repeat('*', 3), -3, null);
+
+//var_dump($_POST);
+
 //var_dump($paragraph);
 
 //var_dump($paragraph_leng);
 
-echo "$paragraph $paragraph_leng";
+//echo `$paragraph ha una lunghezza di $paragraph_leng caratteri`;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Word Result</title>
+</head>
+
+<body>
+    <h1>Hai scritto <?php echo $paragraph ?></h1>
+    <h3>che ha una lunghezza di <?php echo $paragraph_leng ?> caratteri</h3>
+
+    <h3>La parolaccia nascosta è <?php echo $hidden_bad_word ?></h3>
+
+    <h2><?php echo "$paragraph $hidden_bad_word" ?></h2>
+
+</body>
+
+</html>
